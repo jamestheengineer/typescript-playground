c9624402d8a480b7d64a3899c981bb755a77df89
jamestheengineer <jamestheengineer@gmail.com>
2022-12-08 18:50:36 -0500

    Learn some more

--- helloworld.ts
+++ helloworld.ts
@@ -1,2 +1,15 @@
 let message: string = 'Hello World';
 console.log(message);
+
+interface Point {
+  x: number;
+  y: number;
+  z: number;
+}
+
+function logPoint(p: Point) {
+  console.log(`${p.x}, ${p.y}, ${p.z}`);
+}
+
+const point = { x: 12, y: 45, z: 50 };
+logPoint(point);
