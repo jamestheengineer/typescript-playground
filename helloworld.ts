31f1f73aab311beb30f8684e6a3cdf0dc4ea2827
jamestheengineer <jamestheengineer@gmail.com>
2022-12-10 13:40:59 -0500

    Testing the type checker

--- helloworld.ts
+++ helloworld.ts
@@ -18,3 +18,10 @@ let v: any = true;
 v = 'string'; // no error as it can be "any" type
 Math.round(v); // no error as it can be "any" type
 console.log(v);
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet('Brendan');
