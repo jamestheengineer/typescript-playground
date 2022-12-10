2282f7c3a4e3760b078044c363ce78a8bfff41eb
jamestheengineer <jamestheengineer@gmail.com>
2022-12-09 19:37:52 -0500

    More typescript

--- helloworld.ts
+++ helloworld.ts
@@ -13,3 +13,8 @@ function logPoint(p: Point) {
 
 const point = { x: 12, y: 45, z: 50 };
 logPoint(point);
+
+let v: any = true;
+v = 'string'; // no error as it can be "any" type
+Math.round(v); // no error as it can be "any" type
+console.log(v);
