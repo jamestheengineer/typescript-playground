e97691ab3cf12b81c6dde936dc70bb0c7d783eed
jamestheengineer <jamestheengineer@gmail.com>
2023-01-06 21:42:59 -0500

    Read only

--- helloworld.ts
+++ helloworld.ts
@@ -189,3 +189,15 @@ interface PaintOptions {
 function paintShape(opts: PaintOptions) {
   // ...
 }
+
+interface SomeType {
+  readonly prop: string;
+}
+
+function doSomething3(obj: SomeType) {
+  // We can read from 'obj.prop'.
+  console.log(`prop has the value '${obj.prop}'.`);
+
+  // But we can't re-assign it.
+  // obj.prop = "hello";
+}
