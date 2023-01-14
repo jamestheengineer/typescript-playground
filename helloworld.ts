f54f9d1f84bba1be8ed10f2778ba80ea0b15a98c
jamestheengineer <jamestheengineer@gmail.com>
2023-01-13 20:31:12 -0500

    Constrained types

--- helloworld.ts
+++ helloworld.ts
@@ -275,3 +275,13 @@ myGenericNumber.zeroValue = 0;
 myGenericNumber.add = function (x, y) {
   return x + y;
 };
+
+// Constraints on types
+interface Lengthwise {
+  length: number;
+}
+
+function loggingIdentity<Type extends Lengthwise>(arg: Type): Type {
+  console.log(arg.length); // Now we know it has a .length property, so no more error
+  return arg;
+}
