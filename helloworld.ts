fd4f0f7c63f853feeacb7605b396690cadd55dd3
jamestheengineer <jamestheengineer@gmail.com>
2023-01-16 18:22:59 -0500

    More Classes

--- helloworld.ts
+++ helloworld.ts
@@ -332,3 +332,24 @@ class GoodGreeter {
     this.name = 'hello';
   }
 }
+
+class OKGreeter {
+  // Not initialized, but no error
+  name!: string;
+}
+
+class Greeter {
+  readonly name: string = 'world';
+
+  constructor(otherName?: string) {
+    if (otherName !== undefined) {
+      this.name = otherName;
+    }
+  }
+
+  err() {
+    // this.name = "not ok";
+  }
+}
+const g = new Greeter();
+// g.name = "also not ok";
