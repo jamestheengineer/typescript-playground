9a10c0d479d8bb864b5d334573537ee40c8bcd93
jamestheengineer <jamestheengineer@gmail.com>
2023-01-19 18:23:06 -0500

    Extending classes

--- helloworld.ts
+++ helloworld.ts
@@ -391,3 +391,27 @@ const d = new Dog();
 d.move();
 // Derived class method
 d.woof(3);
+
+interface Animal {
+  dateOfBirth: any;
+}
+
+interface Dog extends Animal {
+  breed: any;
+}
+
+class AnimalHouse {
+  resident: Animal;
+  constructor(animal: Animal) {
+    this.resident = animal;
+  }
+}
+
+class DogHouse extends AnimalHouse {
+  // Does not emit JavaScript code,
+  // only ensures the types are correct
+  declare resident: Dog;
+  constructor(dog: Dog) {
+    super(dog);
+  }
+}
