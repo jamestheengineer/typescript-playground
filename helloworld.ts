b21ad500fa7aff2813760436d10d2fbfa70a6a68
jamestheengineer <jamestheengineer@gmail.com>
2023-01-23 19:10:50 -0500

    Modules

--- helloworld.ts
+++ helloworld.ts
@@ -447,3 +447,15 @@ class Employee {
 
 // OK
 const p: Person = new Employee();
+
+// Export. this changes this whole thing into a module not in global space
+export var pi = 3.14;
+export let squareTwo = 1.41;
+export const phi = 1.61;
+
+export class RandomNumberGenerator {}
+
+export function absolute(num: number) {
+  if (num < 0) return num * -1;
+  return num;
+}
