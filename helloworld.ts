c3d6b9cd2dd97f1140ef40b37596a70b58981593
jamestheengineer <jamestheengineer@gmail.com>
2023-01-24 19:29:44 -0500

    Omit types

--- helloworld.ts
+++ helloworld.ts
@@ -459,3 +459,20 @@ export function absolute(num: number) {
   if (num < 0) return num * -1;
   return num;
 }
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: number;
+}
+
+type TodoPreview = Omit<Todo, 'description'>;
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+  createdAt: 1615544252770,
+};
+
+todo;
