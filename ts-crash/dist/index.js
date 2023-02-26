89791b277fee7d77e4c53f35a9ea0ea6a5ba0bb9
jamestheengineer <jamestheengineer@gmail.com>
2023-02-26 09:36:45 -0500

    Learn some more TS

--- ts-crash/dist/index.js
+++ ts-crash/dist/index.js
@@ -1,3 +1,41 @@
 "use strict";
+// Basic types
 let id = 5;
-console.log('ID:', id);
+let company = 'Whoa whoa';
+let isPublished = true;
+let x = 'Hello';
+x = 6;
+let age;
+age = 30;
+let ids = [1, 2, 3, 4, 5];
+let arr = [true, 1, 'string'];
+// Tuple
+let person = [1, 'John', false];
+let employee;
+employee = [
+    [1, 'Joe'],
+    [2, 'Sarah'],
+    [3, 'Johnboy'],
+];
+// Unions
+let pid = 22;
+pid = 'three';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John',
+};
--- ts-crash/src/index.ts
+++ ts-crash/src/index.ts
@@ -1,3 +1,51 @@
+// Basic types
 let id: number = 5;
+let company: string = 'Whoa whoa';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+x = 6;
+let age: number;
+age = 30;
 
-console.log('ID:', id);
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [true, 1, 'string'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'John', false];
+let employee: [number, string][];
+
+employee = [
+  [1, 'Joe'],
+  [2, 'Sarah'],
+  [3, 'Johnboy'],
+];
+
+// Unions
+let pid: string | number = 22;
+pid = 'three';
+
+// Enum
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+// Objects
+type User = {
+  id: number;
+  name: string;
+};
+
+const user: User = {
+  id: 1,
+  name: 'John',
+};
