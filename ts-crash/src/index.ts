f699e80b3a17094f04b151381be53f9def3c57a8
jamestheengineer <jamestheengineer@gmail.com>
2023-02-28 20:13:35 -0500

    Interfaces

--- ts-crash/src/index.ts
+++ ts-crash/src/index.ts
@@ -63,3 +63,28 @@ function addNum(x: number, y: number): number {
 function log(message: string | number): void {
   console.log(message);
 }
+
+// Interfaces
+interface UserInterface {
+  readonly id: number;
+  name: string;
+  age?: number;
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John',
+};
+
+// Can't do this with interfaces
+type Point = number | string;
+const p1: Point = 1;
+
+// user1.id = 1
+
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
